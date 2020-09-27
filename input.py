fa5b403de78dbbc45a4a21bb7b3abca64bedc8a7
Raman <profazpcb@gmail.com>
2020-09-27 11:45:51 -0400

    files added, input.py updated
    added many python files for moderation I think and made input.py even better than last week. :)

--- input.py
+++ input.py
@@ -4,8 +4,13 @@ x = random.randint(1,100)
 user_input = int(-1)
 
 while user_input != x:
-    user_input = int(raw_input("Type something to test this out: "))
+    user_input = input("Type something to test this out: ")
     if user_input < x:
         print('number is higher')
+    if user_input > x:
+        print('number is lower')
+
+    if user_input == x:
+        print('YOU GOT THE NUMBER!!!')
     else:
-        print('below 50')
+        print('wrong number')
