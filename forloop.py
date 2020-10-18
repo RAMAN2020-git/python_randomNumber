9a9e2eeb04ea9bbefe7953c7acf5b504afbae543
Raman <profazpcb@gmail.com>
2020-10-18 12:56:58 -0400

    Updated forloop.py

--- forloop.py
+++ forloop.py
@@ -1,4 +1,12 @@
-fib = ["1","11","111"]
+#fib = ["        |1|    ", "       |1|1|", "      |1|2|1|", "     |1|3|3|1|", "    |1|4|6|4|1|", "  |1|5|10|10|5|1|"]
 
-for x in range(3):
-	    print(fib)
+fib = [1]
+#print(fib)
+
+fib = [1,2,1]
+#print(fib)
+
+fib_temp = []
+#for i in range(2):
+for j in range(0,len(fib)-1):
+	print(fib[j])
